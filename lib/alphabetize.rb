0a8f6011776905784da2f510f81dcc75162f9e56
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-10-21 01:16:45 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -8,5 +8,10 @@ def alphabetize(arr)
   # Compare the array by changing its values to ones we can more easily compare 
   # so change from esperanto to any ascii code in order and same length
   # and then regular sort by comparing
-  arr.sort_by{|phrase1| phrase1.tr(esperanto_alphabet, ascii_equivalent)}
+  # How this sort works is it's basically telling the array that you want
+  # to sort the array instead of regular ascii order, that you want to
+  # sort based on the translated phrase, not regular phrase.
+  # It will then compare the phrases and put them in order based on the
+  # translated phrase order but return the original phrases
+  arr.sort_by{|phrase| phrase.tr(esperanto_alphabet, ascii_equivalent)}
 end
